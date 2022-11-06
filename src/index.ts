6a5246a91a3f285b4477260e30af37b995463142
Joanna Liana <joanna_liana@outlook.com>
2022-11-06 18:30:40 +0100

    Extract main section handler

--- src/index.ts
+++ src/index.ts
@@ -109,55 +109,31 @@ const packTree: StorageHandler = (storage: JewelleryStorage, item: Jewellery) =>
   return Result.Skipped;
 }
 
+const packMainSection: StorageHandler = (storage: JewelleryStorage, item: Jewellery) => {
+  if (item._kind === "Earring" && item.stone === "Plain") {
+    storage.box.mainSection.push(item);
+
+    return Result.Success;
+  }
+
+  return Result.Skipped;
+}
+
 export function pack(item: Jewellery, storage: JewelleryStorage) {
   const baseHandler: StorageHandler = (storage, item) => {
-    if (item._kind === "Earring") {
-      if (item.stone === "Plain") {
-        storage.box.mainSection.push(item);
-      }
-    } else {
-      storage.dresserTop.push(item);
-    }
+    storage.dresserTop.push(item);
 
     storage.travelRoll = storage.travelRoll.filter((x) => x !== item);
 
     return Result.Success;
   }
 
-  const next2 = chain(packTree, baseHandler);
+  const next3 = chain(packMainSection, baseHandler);
+  const next2 = chain(packTree, next3);
   const next1 = chain(packSafe, next2);
   const chainRoot = chain(packTopShelf, next1);
 
   return chainRoot(storage, item);
-}
-
-const packMainSection = (storage: JewelleryStorage, item: Jewellery) => {
-  if (storage.travelRoll.includes(item) && item.size() !== "Large")
-    storage.box.topShelf.push(item);
-  else if (item.stone === "Diamond") {
-    storage.safe.push(item);
-  } else if (item.size() === "Small") {
-    storage.box.topShelf.push(item);
-  } else if (item._kind === "Earring") {
-    if (item.type === "Hoop") {
-      storage.tree.push(item);
-    } else if (item.stone === "Plain") {
-      storage.box.mainSection.push(item);
-    } else {
-      storage.box.topShelf.push(item);
-    }
-  } else if (item._kind === "Necklace") {
-    if (item.type === "Pendant") {
-      storage.tree.push(item.chain);
-      storage.box.topShelf.push(item.pendant);
-    } else {
-      storage.tree.push(item);
-    }
-  } else {
-    storage.dresserTop.push(item);
-  }
-
-  storage.travelRoll = storage.travelRoll.filter((x) => x !== item);
 }
 
 const packDresserTop = (storage: JewelleryStorage, item: Jewellery) => {
